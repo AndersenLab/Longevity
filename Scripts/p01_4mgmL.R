d8f643f17fa4f83453b05eaad82ce3c9e5b628d1
Ryan Abdella <RyanAbdella2019@u.northwestern.edu>
2015-01-30 10:20:12 -0600

    First trial outputs everything correctly, there's an issue with the rendering of the markdown reports though.

--- Scripts/p01_4mgmL.R
+++ Scripts/p01_4mgmL.R
@@ -1,8 +1,14 @@
 # Example input file to Longevity R Markdown
 
 replicates <- 8
-data_points_per_day <- 4
+num_bins <- 4
 plateID <- "Longevity_1_4"
 strains <- c("CB4856", "CX11314", "ED3017", "JT11398", "DL238", "N2", "MY23", "JU258", "EG4725",  "LKC34", "JU775", "MY16")
 nstrains <- length(strains)
 colRemove <- c(5, 6)
+
+#bin_size <- as.numeric(str_split(read.delim(file = data.loc, head = FALSE, skip = 5)[1, ], " ")[[1]][2])
+#time <- as.numeric(str_split(read.delim(file = data.loc, head = FALSE, skip = 5)[1, ], " ")
+#[[1]][length(str_split(read.delim(file = data.loc, head = FALSE, skip = 5)[1, ], " ")[[1]])])
+#num_groups <- find_num_groups(data.loc)
+#num_bins <- time / bin_size
--- Scripts/p03_2mgmL.R
+++ Scripts/p03_2mgmL.R
@@ -4,7 +4,7 @@ replicates <- 8
 plateID <- "Longevity_3_2"
 strains <- c("CB4856", "CX11314", "ED3017", "JT11398", "DL238", "N2", "MY23", "JU258", "EG4725",  "LKC34", "JU775", "MY16")
 nstrains <- length(strains)
-colRemove <- c(1, 3)
+colRemove <- c(1, 3, 5, 6, 10, 12)
 
 bin_size <- as.numeric(str_split(read.delim(file = data.loc, head = FALSE, skip = 5)[1, ], " ")[[1]][2])
 time <- as.numeric(str_split(read.delim(file = data.loc, head = FALSE, skip = 5)[1, ], " ")
