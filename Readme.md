2ee87e5c516ee18d9fb8a96b932e9a75bee771a3
Ryan Abdella <RyanAbdella2019@u.northwestern.edu>
2015-02-04 18:12:15 -0600

    Added fitting best fit curves per well and some code reorganization

--- Readme.md
+++ Readme.md
@@ -2,11 +2,22 @@ wMicrotracker Scoring
 =====================
 
 ## Food Optimization
-- [ ] 
+
+- [X] Specify entire columns to exclude 
+- [ ] Specify individual wells to exclude
+- [ ] Comment code
+- [ ] Move functions to separate file
+- [ ] Move all library loads to the same file
 
 
 
 ## Curve Fits
+
 - [X] `nlm` for regression
 - [ ] `nls` for regression
-	- `SelfStart` 
+	- [ ] `SelfStart` 
+- [ ] fit smooth curves to data
+	- [ ] fit logistic to smooth curve
+- [X] graph `nlm` fits per strain
+- [X] graph `nlm` fits per well
+	
---
+++ Scripts/survival_fxns.R
@@ -0,0 +1,31 @@
+well_gompertz <- function(input, df, row, strain) {
+  sum(((100 * 100 ^ ((input[1] / input[2]) * (1 - exp(input[2] * df$day[df$row == row & df$strain == strain])))) - df$norm.act[df$row == row & df$strain == strain]) ^ 2)
+}
+
+strain_gompertz <- function(input, df, strain) {
+  sum(((100 * 100 ^ ((input[1] / input[2]) * (1 - exp(input[2] * df$day[df$strain == strain])))) - df$norm.act[df$strain == strain]) ^ 2)
+}
+
+well_weibull <- function(input, df, row, strain) {
+  sum(((100 * 100 ^ (-((df$day[df$row == row & df$strain == strain] / input[2]) ^ input[1]))) - df$norm.act[df$row == row & df$strain == strain]) ^ 2)
+}
+
+strain_weibull <- function(input, df, strain) {
+  sum(((100 * 100 ^ (-((df$day[df$strain == strain] / input[2]) ^ input[1]))) - df$norm.act[df$strain == strain]) ^ 2)
+}
+
+well_twoplog <- function(input, df, row, strain) {
+  sum(((100 / (1 + ((df$day[df$row == row & df$strain == strain] / input[2]) ^ input[1]))) - df$norm.act[df$row == row & df$strain == strain]) ^ 2)
+}
+
+strain_twoplog <- function(input, df, strain) {
+  sum(((100 / (1 + ((df$day[df$strain == strain] / input[2]) ^ input[1]))) - df$norm.act[df$strain == strain]) ^ 2)
+}
+
+well_threeplog <- function(input, df, row, strain) {
+  sum(((100 * 100 ^ (-input[2] * df$day[df$row == row & df$strain == strain] - (input[2] / input[3]) * ln((input[1] - (input[2] - input[1]) * exp(-input[3] * df$day[df$row == row & df$strain == strain])) / input[2]))) - df$norm.act[df$row == row & df$strain == strain]) ^ 2)
+}
+
+strain_threeplog <- function(input, df, strain) {
+  sum(((100 * 100 ^ (-input[2] * df$day[df$strain == strain] - (input[2] / input[3]) * ln((input[1] - (input[2] - input[1]) * exp(-input[3] * df$day[df$strain == strain])) / input[2]))) - df$norm.act[df$strain == strain]) ^ 2)
+}
