c1d9d0016ce5d12cc8975ed94068f87d320f6efe
Ryan Abdella <RyanAbdella2019@u.northwestern.edu>
2015-02-20 18:11:48 -0600

    Removed col and row factors

--- Scripts/p01_2mgmL.R
+++ Scripts/p01_2mgmL.R
@@ -1,16 +1,9 @@
 # Example input file to Longevity R Markdown
 
 replicates <- 8
-numBins <- 4
 strains <- c("CB4856", "CX11314", "ED3017", "JT11398", "DL238", "N2", "MY23", "JU258", "EG4725",  "LKC34", "JU775", "MY16")
 rows <- c("A", "B", "C", "D", "E", "F", "G", "H")
 nStrains <- length(strains)
 colRemove <- c(2, 5, 6)
 wellRemove <- data.frame(col = c(4), row = c("E"))
 levels(wellRemove$row) <- rows
-
-#bin_size <- as.numeric(str_split(read.delim(file = data.loc, head = FALSE, skip = 5)[1, ], " ")[[1]][2])
-#time <- as.numeric(str_split(read.delim(file = data.loc, head = FALSE, skip = 5)[1, ], " ")
-                    #[[1]][length(str_split(read.delim(file = data.loc, head = FALSE, skip = 5)[1, ], " ")[[1]])])
-#num_groups <- find_num_groups(data.loc)
-#num_bins <- time / bin_size
